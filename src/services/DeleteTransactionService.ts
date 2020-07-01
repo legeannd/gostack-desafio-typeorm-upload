8b1d059cc93290b6782d0e8c6acb96017042d686
Gean Lucas <gean.lfpsa@gmail.com>
2020-07-01 01:13:56 -0300

    Importando arquivo e adicionando ao banco de dados

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,27 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    try {
+      const transaction = await transactionRepository.findOne({
+        where: {
+          id,
+        },
+      });
+      if (transaction) {
+        await transactionRepository.delete(transaction.id);
+      }
+    } catch (err) {
+      throw new AppError('Unable to delete transaction');
+    }
   }
 }
 
